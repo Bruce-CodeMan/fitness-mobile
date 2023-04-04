c5bdbc62623dcf70e18c91ea4e4ab2a699831a46
Bruce-CodeMan <1033684650@qq.com>
2023-04-04 16:15:50 +0800

    update theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@
  * @Author: Bruce
  * @Description: 
  */
-import './App.css'
-import { useQuery, useMutation } from '@apollo/client'
-import { FIND, UPDATE } from './graphql/demo'
-import React, { useState } from 'react'
+import './App.css';
+import { useQuery, useMutation } from '@apollo/client';
+import { FIND, UPDATE } from './graphql/demo';
+import { 
+  Form,
+  Input,
+  Button
+ } from 'antd-mobile';
 
 const App = () => {
-
-  const [name, setName] = useState('');
-  const [desc, setDesc] = useState('');
 
   const {loading, data} = useQuery(FIND, {
     variables: {
@@ -21,21 +22,12 @@ const App = () => {
 
   const [ update ] = useMutation(UPDATE)
 
-  const onChangeNameHandler = (v: React.ChangeEvent<HTMLInputElement>) => {
-    setName(v.target.value);
-  }
-
-  const onChangeDescHandler = (v: React.ChangeEvent<HTMLInputElement>) => {
-    setDesc(v.target.value);
-  }
-
-  const onClickHandler = () => {
+  const onClickHandler = (v:any) => {
     update({
       variables: {
         "id": "9becbed5-6474-452c-a277-b59784714a68",
         "params": {
-          name,
-          desc
+          ...v,
         }
       }
     })
@@ -45,15 +37,28 @@ const App = () => {
     <p> data: {JSON.stringify(data)}</p>
     <p>loading: {`${loading}`}</p>
 
-    <p>
-      name: <input type="text" onChange={onChangeNameHandler}/>
-    </p>
-    <p>
-      desc: <input type="text" onChange={onChangeDescHandler}/>
-    </p>
-    <p>
-      <button onClick={onClickHandler}>Click</button>
-    </p>
+    <Form
+      layout="horizontal"
+      onFinish={onClickHandler}
+      footer={(
+        <Button block type='submit' color='primary' size='large'>
+          提交
+        </Button>
+      )}
+    >
+      <Form.Item
+        name="name"
+        label="姓名"
+      >
+        <Input/>
+      </Form.Item>
+      <Form.Item
+        name="desc"
+        label="描述"
+      >
+        <Input/>
+      </Form.Item>
+    </Form>
   </div>
 }
 
