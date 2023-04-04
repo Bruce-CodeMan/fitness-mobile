091d4c6b99ef20c597627837d539ef547f6c8f96
Bruce-CodeMan <1033684650@qq.com>
2023-04-04 11:07:05 +0800

    create a folder to export a client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+/*
+ * @Date: 2023-04-04 10:12:00
+ * @Author: Bruce
+ * @Description: 
+ */
+
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './utils/apollo';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
---
+++ src/utils/apollo.ts
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2023-04-04 10:23:32
+ * @Author: Bruce
+ * @Description: 
+ */
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache(),
+})
